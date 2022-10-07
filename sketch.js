c89331a3f59d0e093548d8da8f8c89ce59902b62
Darzek1 <115124355+Darzek1@users.noreply.github.com>
2022-10-06 21:20:08 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,63 +2,29 @@ let boardSize;
 let canvas;
 let hunter;
 let isRed
+let countingDirection;
+let isCountingRight;
 function setup(){
-  isRed = false;
+  textAlign(CENTER, CENTER);
+  isRed = true;
   boardSize = min(windowWidth, windowHeight);
   canvas = createCanvas(boardSize, boardSize);
   canvas.position(windowWidth/4, 0);
-  console.log("windowWidth"+windowWidth)
-  console.log("windowHeight"+windowHeight)
+  console.log("windowWidth"+windowWidth);
+  console.log("windowHeight"+windowHeight);
+  isCountingRight = true;
+  countingDirection = "left";
 }
 
 function draw(){
 
-  background(255,0,0);
-  strokeWeight(5);
-
-  for (let y = 0; y < 10; y++) { 
-
-      isRed = !isRed;
-
-    for (let x = 0; x < 10; x++) {
-
-      isRed = !isRed;
-      fill(255)
-
-      if (isRed){
-        fill(255, 0, 0)
-      }
-      
-      square(x*boardSize/10, y*boardSize/10, boardSize/10);
-      
-    }
-}
-
-
-
-
-
-
-  // drawLine(10, 10);
+  drawGriddy();
   
-//   for (let s = 0; s <= 10; s++) {
-//     const element = hunter[s];
-//  drawLine
-//   }
-  // CREATE GRID USING:
 
-  // square(x, y, s); + Lists + for loops
 
-  // look up nested for loops javascript if you're confused
-
-  // CLASSES (don't worry about it unless you want to for now)
-  // Snake.move()
-  // Snake.draw()
-  // Snake.location
+  strokeWeight(2);
 
 }
-
-
 function printList(){
 
   for (let i = 0; i < hunter.length; i++) {
@@ -68,27 +34,81 @@ function printList(){
     
   }
 }
+function drawGriddy(){
+  // CREATE GRID USING:
 
-function drawLine(_row,_col) {
-  rowHeight = windowHeight/_row;
-  columnWidth = windowWidth/_col;
-  posX = 0
-  posY = 0
-  posX2 = 0
-  posY2 = 0
-for (let row = 0; row <= _row; row++) { 
-  posX2 = windowWidth
-line(posX, posY, posX2, posY2)
-posY += rowHeight
-posY2 += rowHeight
+  // square(x, y, s); + Lists + for loops
+
+  // look up nested for loops javascript if you're confused
+  for (let y = 0; y < 10; y++) {
+
+    isCountingRight = !isCountingRight;
+  
+    isRed = !isRed
+    for (let x = 0; x < 10; x++) {
+  
+      if (isRed){
+        fill(255, 0, 0);
+    }
+      square(x*boardSize/10, y*boardSize/10, boardSize/10);
+    
+      isRed = !isRed;
+      fill(0);
+  
+      if(isCountingRight){
+        text(100 - (y*10 + 9 - x), x*boardSize/10 + boardSize/20, y*boardSize/10 + boardSize/20);
+      }
+      else{
+        text(100 - (y*10 + x), x*boardSize/10 + boardSize/20, y*boardSize/10 + boardSize/20);
+    }
+    
+      // 100 - (y*10 + x) = count from left to right side
+  
+      // figure out how to count from right to left HINT: y*10 does not need changing
+  
+      fill(255)
+  textSize(50)
+    }
+    }
 }
-posX = 0
-posY = 0
-posX2 = 0
-posY2 = 0
-for (let col = 0; col <= _col; col++) { 
-  posY2 = windowHeight
-  line(posX, posY, posX2, posY2)
-  posX += columnWidth
-  posX2 += columnWidth
-}}
+
+
+function hitGriddy(x, y){
+  // converts X and Y coordinates to points on a 10 by 10 grid, then converts the points into a tile number
+  let hitGriddyX;
+  let hitGriddyY;
+  
+  hitGriddyX = ceil(map(x, 0, width, 0, 10));
+  hitGriddyY = ceil(map(y, 0, height, 10, 0));
+  
+  if (hitGriddyY % 2 === 0){
+    currentGriddy = (hitGriddyY - 1)* 10 + (10 - (hitGriddyX - 1));
+  }
+  else{
+    currentGriddy = (hitGriddyY - 1)* 10 + hitGriddyX;
+  }
+  // return [hitGriddyX, hitGriddyY];
+  return currentGriddy;
+}
+// tile number --> [1,1] to [10, 10] points --> [x, y]
+function griddyHit(griddy){
+let secondGriddy;
+let thirdGriddy;
+//secondGriddy = Number(str(griddy)[str(griddy).length - 1]);
+secondGriddy = Number(str(griddy)[1]);
+thirdGriddy = Number(str(griddy)[0]);
+print(secondGriddy);
+print(thirdGriddy);
+return [secondGriddy, thirdGriddy];
+}
+function mouseClicked(){
+
+  print(hitGriddy(mouseX, mouseY));
+  print(griddyHit(hitGriddy(mouseX, mouseY)));
+}
+
+// 4 L8R
+// CLASSES (don't worry about it unless you want to for now)
+// Snake.move()
+// Snake.draw()
+// Snake.location
